26cb76f1134bf4a082abc2305cabb6457e5dd5ab
divyamsingh007 <legendprice007@gmail.com>
2025-12-25 01:38:12 +0530

    Completed HeroSection and Started BlogSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,34 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Rellax from "rellax";
 import Background from "../components/Background";
 import Navbar from "../components/Navbar";
 
 import CurvedLoop from "../ui/CurvedLoop";
 import BlurText from "../ui/BlurText";
 import ShinyText from "../ui/ShinyText";
+import TextPressure from "../ui/TextPressure";
 import ProfileCard from "../ui/ProfileCard";
 
 function Home() {
   const handleAnimationComplete = () => {
     console.log("Animation completed!");
   };
+
+  useEffect(() => {
+    const rellax = new Rellax(".rellax", {
+      speed: -3,
+      center: false,
+      wrapper: null,
+      round: true,
+      vertical: true,
+      horizontal: false,
+    });
+
+    return () => {
+      rellax.destroy();
+    };
+  }, []);
 
   return (
     <>
@@ -109,8 +128,20 @@ function Home() {
                     marginTop: "clamp(14px, 1.2vw, 16px)",
                   }}
                 />
+                <div className="cta-container flex gap-6">
+                  <Link to="https://divyamsingh.me/">
+                    <button className="line-btn">
+                      <h6 className="italic">Portfolio</h6>
+                    </button>
+                  </Link>
+                  <Link to="#">
+                    <button className="line-btn">
+                      <h6 className="italic">To The Blogs</h6>
+                    </button>
+                  </Link>
+                </div>
               </div>
-              <div className="img-box">
+              <div className="img-box rellax" data-rellax-speed="-3">
                 <ProfileCard
                   name="Divyam Singh"
                   title="Thinking in components"
@@ -127,6 +158,51 @@ function Home() {
             </div>
           </div>
         </div>
+      </section>
+      <section className="blog-section px-4 sm:px-6 md:px-10 lg:px-16 xl:px-20 2xl:px-24 pt-4 md:pt-6 lg:pt-10 pb-6 md:pb-8 lg:pb-12 h-1000">
+        <h1 style={{ margin: 0 }}>
+          <div
+            style={{
+              position: "relative",
+              height: "auto",
+              textDecoration: "none",
+            }}
+          >
+            <TextPressure
+              text="From My Desk!
+              To You!!"
+              flex={true}
+              alpha={false}
+              stroke={false}
+              width={true}
+              weight={false}
+              italic={true}
+              textColor="#2B1F39"
+              strokeColor="#ff0000"
+              minFontSize={42}
+            />
+          </div>
+        </h1>
+        <hr style={{ marginTop: 0 }} />
+        <BlurText
+                  text="Things I’m Figuring Out"
+                  delay={150}
+                  animateBy="words"
+                  direction="top"
+                  onAnimationComplete={handleAnimationComplete}
+                  className=""
+                  style={{
+                    color: "#2B1F39",
+                    fontFamily: "Montserrat, sans-serif",
+                    fontOpticalSizing: "auto",
+                    fontWeight: "bold",
+                    fontStyle: "normal",
+                    fontSize: "clamp(2.25rem, 5vw + 1rem, 5.6rem)",
+                    lineHeight: "144px",
+                    textAlign: "left",
+                    opacity: 1,
+                  }}
+                />
       </section>
     </>
   );
