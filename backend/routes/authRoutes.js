4e190dc36854cf4f350d6bf9f202c0fb63d42fee
divyamsingh007 <legendprice007@gmail.com>
2026-03-23 23:22:47 +0530

    security flaw patched

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,6 @@ const { loginUser, registerUser } = require('../controllers/authController');
 
 router.post('/login', loginUser);
 // Note: In production you might want to protect or remove the register route
-router.post('/register', registerUser);
+// router.post('/register', registerUser);
 
 module.exports = router;
